018f5e507efb8ad87dc88c1a22de44545932d74e
Sywice <m1rzelo5756@gmail.com>
2026-03-20 22:48:21 +0700

    update: post-запрос на эндпоинт answer

--- app/api_client.py
+++ app/api_client.py
@@ -1,15 +1,17 @@
+import aiohttp
+
 class Backend_Client():
 
     def __init__(self, base_url):
         self.base_url = base_url
 
-    async def send_message(self, user_id: int, text: str, interview_id : int):
+    async def send_message(self, user_id: int, answerText: str, interview_id : int):
         """Метод для отправки сообщения на бэкенд и получения ответа"""
         async with aiohttp.ClientSession() as session:
-            payload = {"user_id": user_id, "interview_id":  interview_id, "text": text}
+            payload = {"answerText" : answerText}
             
-            # Делаем POST-запрос на эндпоинт /chat
-            async with session.post(f"{self.base_url}/chat", json=payload) as response:
+            # Делаем POST-запрос на эндпоинт Answer Question
+            async with session.post(f"{self.base_url}/interviews/answer/{interview_id}", json=payload) as response:
                 # Ждем, пока бэкенд ответит и превращаем ответ в словарь (dict)
                 result = await response.json()
                 # Возвращаем только текст ответа от нейронки/бэкенда
